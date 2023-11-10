33fd25bc6c281a3340048b1b3a66f248f4562067
juliette <juliette.tardy@gmail.com>
2023-11-10 11:40:32 +0100

    enfin 3 lignes d'aliens

--- TP4.py
+++ TP4.py
@@ -23,17 +23,14 @@ Canevas = Canvas(window, width = width_canvas, height = height_canvas, bg = 'gra
 Canevas.pack(padx = 10, pady = 10)
 
 # ------------ Création des aliens ------------
-# Coordonnées X,Y des anneaux :
-
-
 # Couleurs des anneaux :
 coul_for6 = ["red", "yellow", "blue", "green", "black", "purple"]
 coul_for5 = ["pink", "orange", "brown", "white", "cornflowerblue"]
 
-
 height_aliens = 30
-for nmb_lines in range(1,3) :
+for nmb_lines in range(1,4) :
     if nmb_lines%2 != 0 :
+        # Coordonnées X,Y des anneaux :
         place_for6 = width_canvas/6 
         final_place_for6 = place_for6 - 20 - width_canvas/12
         coord_for6 = [[final_place_for6, height_aliens], [final_place_for6 + place_for6, height_aliens], [final_place_for6 + 2*place_for6, height_aliens], [final_place_for6 + 3*place_for6, height_aliens], [final_place_for6 + 4*place_for6, height_aliens], [final_place_for6 + 5*place_for6, height_aliens]]
@@ -43,8 +40,9 @@ for nmb_lines in range(1,3) :
             Canevas.create_oval(x0, y0, x0+40, y0+40, width = 2, outline = 'black', fill = coul_for6[i])
             i += 1
     else : 
-        place_for5 = width_canvas/5
-        final_place_for5 = place_for5 - 20 - width_canvas/10
+        # Coordonnées X,Y des anneaux :
+        place_for5 = width_canvas/6
+        final_place_for5 = place_for5 - 20 
         coord_for5 = [[final_place_for5, height_aliens], [final_place_for5 + place_for5, height_aliens], [final_place_for5 + 2*place_for5, height_aliens], [final_place_for5 + 3*place_for5, height_aliens], [final_place_for5 + 4*place_for5, height_aliens]]
         i = 0
         while i < 5 :
@@ -53,8 +51,6 @@ for nmb_lines in range(1,3) :
             i += 1
     height_aliens += 80
     
-    
-
 # ------------ Création du vaisseau ------------
 
 # Position initiale du vaisseau
