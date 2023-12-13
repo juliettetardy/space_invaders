e118ca98b2f8120631b999251166de7e98ccc569
juliette <juliette.tardy@gmail.com>
2023-12-13 23:46:58 +0100

    the best i can do pour les aliens pour les l'instant

--- invaders.py
+++ invaders.py
@@ -7,10 +7,10 @@ class Invader :
         self.canevas = canevas
         self.x = x
         self.y = y
-        self.invader_pic = Image.open(img_path)
-        self.invader_pic = self.invader_pic.resize((100,100))
-        self.pic = ImageTk.PhotoImage(self.invader_pic)
-        self.invader_item = self.canevas.create_image(self.x + 20, self.y + 20, image = self.pic)
+        self.invader_pic = Image.open (img_path)
+        self.invader_pic = self.invader_pic.resize ((100, 100))
+        self.pic = ImageTk.PhotoImage (self.invader_pic)
+        self.invader_item = self.canevas.create_image (self.x + 20, self.y + 20, image = self.pic)
 
     def get_position (self) :
         return self.canevas.coords (self.invader_item)
@@ -20,39 +20,39 @@ class Invader :
             self.canevas.move (self.invader_item, speed, 0)
         elif moving == 1 :
             self.canevas.move (self.invader_item, speed, 5)
-            
+
 class Invaders :
     def __init__ (self, window, canevas, imgPath) :
         self.window = window
         self.canevas = canevas
         self.imgPath = imgPath
         self.invaders = []
-        self.speed = 4
+        self.speed = - 4
  
     def add_invaders (self) :
         width_canvas = 1530
         height_aliens = 30
-        for nmb_lines in range(1,4) :
-            if nmb_lines%2 != 0 :
+        for nmb_lines in range (1, 4) :
+            if nmb_lines % 2 != 0 :
                 # Coordonnées X,Y de chqaue alien :
-                place_for5 = width_canvas/5
-                final_place_for5 = place_for5 - 20 - width_canvas/10
-                coord_for5 = [[final_place_for5, height_aliens], [final_place_for5 + place_for5, height_aliens], [final_place_for5 + 2*place_for5, height_aliens], [final_place_for5 + 3*place_for5, height_aliens], [final_place_for5 + 4*place_for5, height_aliens]]
+                place_for5 = width_canvas / 5
+                final_place_for5 = place_for5 - 20 - width_canvas / 10
+                coord_for5 = [[final_place_for5, height_aliens], [final_place_for5 + place_for5, height_aliens], [final_place_for5 + 2 * place_for5, height_aliens], [final_place_for5 + 3 * place_for5, height_aliens], [final_place_for5 + 4 * place_for5, height_aliens]]
                 i = 0
                 while i < 5 :
-                    x0, y0 = coord_for5[i][0], coord_for5[i][1]
-                    invader = Invader(x0, y0, self.canevas, self.imgPath)
+                    x0, y0 = coord_for5 [i][0], coord_for5 [i][1]
+                    invader = Invader (x0, y0, self.canevas, self.imgPath)
                     self.invaders.append (invader)
                     i += 1
             else : 
                 # Coordonnées X,Y de chaque alien :
-                place_for4 = width_canvas/5
+                place_for4 = width_canvas / 5
                 final_place_for4 = place_for4 - 20 
-                coord_for4 = [[final_place_for4, height_aliens], [final_place_for4 + place_for4, height_aliens], [final_place_for4 + 2*place_for4, height_aliens], [final_place_for4 + 3*place_for4, height_aliens]]
+                coord_for4 = [[final_place_for4, height_aliens], [final_place_for4 + place_for4, height_aliens], [final_place_for4 + 2 * place_for4, height_aliens], [final_place_for4 + 3 * place_for4, height_aliens]]
                 i = 0
                 while i < 4 :
-                    x0, y0 = coord_for4[i][0], coord_for4[i][1]
-                    invader = Invader(x0, y0, self.canevas, self.imgPath)
+                    x0, y0 = coord_for4 [i][0], coord_for4 [i][1]
+                    invader = Invader (x0, y0, self.canevas, self.imgPath)
                     self.invaders.append (invader)
                     i += 1
             height_aliens += 80
@@ -64,11 +64,12 @@ class Invaders :
                 break
             
         for invader in self.invaders :
-            if self.invaders[0].get_position() [0] - 40 <= 0 or self.invaders[9].get_position() [0] - 40 <= 0 :
+            if self.invaders [4].get_position() [0] + 40 >= 1530 and self.invaders [-1].get_position() [0] + 40 >= 1530 :
                 invader.invaders_move (self.speed, 1)
-            invader.invaders_move (self.speed, 0)
+            else :
+                invader.invaders_move (self.speed, 0)
 
         self.window.after(20, self.move_invaders)
-   
+
 #This means that when you update the y-coordinate of the invader by subtracting 20 from it, the actual position of the invader in the window does not change because the position of the invader's image relative to the window remains the same.
 
