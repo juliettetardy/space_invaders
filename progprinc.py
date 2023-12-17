172ecf43e91ec52adbd099fac27efadaf95d995b
juliette <juliette.tardy@gmail.com>
2023-12-17 16:37:47 +0100

    maj

--- progprinc.py
+++ progprinc.py
@@ -12,7 +12,8 @@ Il reste à faire :
                             20 points si c'est un ennemi en capacité de tirer, 
                             150 points pour l'ennemi bonus)
 - Créer un ennemi bonus
-- Bonne pratiques + commentaires fichier ship
+- Bonnes pratiques + commentaires fichier ship
+- Bonnes pratiques + commentaires fichier missile (non terminé)
 - Mettre des règles sur le menu start game
 - Créer un READ ME
 """
