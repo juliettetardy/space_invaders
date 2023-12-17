c6b314eee054a200ab74cc0cb0084e497da16fea
justine <curiejustine@gmail.com>
2023-12-17 20:08:58 +0100

    commiiiiiit

--- ship.py
+++ ship.py
@@ -3,8 +3,63 @@ from missile import Missile_S
 from PIL import Image, ImageTk
 
 class Ship:
+    """
+    Une classe qui permet de controler le vaisseau du joueur.
+    Cette classe s'occupe de tous les éléments graphiques vis à vis du vaisseau et la gestion des mouvements.
+
+    Fonctions 
+    ---------
+        ship_move :
+            Fonction qui permet de déplacer le vaisseau 
+        fire_shoot :
+            Fonction qui permet de déplacer le vaisseau
+        get_score :
+            Fonction qui récupère le score 
+        add_score :
+            Fonction qui ajoute le score du joueur 
+        lost_life :
+            Fonction qui démarre le jeu lorsque l'on est sur l'écran d"accueil
+
+
+    """
     
     def __init__ (self, x, y, window, canevas, width, height, img_path) : 
+        """
+        Initialisateur. 
+        Fonction qui initialise les objets de la classe pour les réutiliser dans les fonctions associées à celle ci.
+        Entrée(s): 
+            x : 
+                Coordonnée en abscisse du vaisseau sur l'écran
+                type = int
+
+            y : 
+                Coordonnées en ordonnée du vaisseau sur l'écran
+                type = int
+
+            window : 
+                fenêtre contenant le jeu
+                type = Tk
+
+            canevas : 
+                canevas du jeu sur lequel le missile va se déplacer
+                type = Canvas
+            
+            width : 
+                Largeur du vaisseau
+                type = int
+
+            height :
+                Hauteur du vaisseau
+                type = int
+
+            img_path :
+                Image du vaisseau 
+                type = str
+
+
+        Sortie(s): None
+
+        """
         self.x = x
         self.y = y
         self.window = window
@@ -15,6 +70,7 @@ class Ship:
         self.score = 0
         self.var_score = None
 
+        # Création de l'image du vaisseau
         self.w = width / 2
         self.h = height - 70
         self.ship_pic = Image.open (img_path)
@@ -23,6 +79,14 @@ class Ship:
         self.player_item = self.canevas.create_image (self.w, self.h, image = self.pic)
 
     def ship_move (self, delta) :
+        """ 
+        Fonction qui permet de déplacer le vaisseau 
+        Entrée(s): 
+            delta : longueur de déplacement du vaisseau à chaque appel de la fonction
+            type = int
+        Sortie(s): None
+
+        """
         coord = self.canevas.coords (self.player_item)
         if len (coord) == 2 :
             new_position = coord [0] + delta
@@ -30,27 +94,68 @@ class Ship:
                 self.canevas.move (self.player_item, delta, 0)
         
     def fire_shoot (self, back_img) :
+        """ 
+        Fonction qui envoie un missile du vaisseau
+        Entrée(s): 
+            back_img: 
+                Image de fond du jeu
+                type = 
+        Sortie(s): None
+
+        """
         coord = self.canevas.coords (self.player_item)
         if len (coord) == 2 :
+            # Création d'un missile
             shot = Missile_S (coord [0] + 7, coord [1] - 80, self.window, self.canevas, self)
             shot.bullet_ship (back_img)
 
     def get_score (self) :
-        # Mettre à jour le texte du label avec la nouvelle valeur du score
+
+        """ 
+        Fonction qui récupère le score 
+        Entrée(s): None
+        Sortie(s): 
+            score :
+                Score du joueur 
+                type = int
+
+        """
         return self.score
 
     def add_score (self, score_to_add) :
-        # Fonction pour ajouter au score
+        """ 
+        Fonction qui ajoute le score du joueur  
+        Entrée(s): 
+            score_to_add :
+                points à ajouter au score du joueur
+                type = int
+        Sortie(s): None
+
+        """
         self.score += score_to_add
         if self.var_score :
             self.var_score.set (self.score)
 
     def lost_life (self) :
+        """ 
+        Fonction qui démarre le jeu lorsque l'on est sur l'écran d"accueil 
+        Entrée(s): None
+        Sortie(s): 
+            0 : 
+                Si c'est la dernière vie du joueur
+                type = int
+            1 :
+                Si le joueur a perdu une vie
+                type = int  
+
+        """
+        # Cas ou le joueur n'as plus qu'une vie
         if self.life == 1 :
             self.life -= 1
             if self.var_life :
                 self.var_life.set (self.life)
             return 1
+        # Cas ou le joueur a encore 2 ou 3 vies
         else :
             self.life -= 1
             if self.var_life :
