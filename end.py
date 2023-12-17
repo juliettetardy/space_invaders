6bbb15b7a3b8fca064e494226cff17bbddd13619
juliette <juliette.tardy@gmail.com>
2023-12-17 16:40:52 +0100

    maj

--- end.py
+++
@@ -1,5 +0,0 @@
-"""
-def end(canvas):
-    canvas.delete('all')
-    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font=('consolas',70), text="END", fill="red", tag="gameover")
-"""
